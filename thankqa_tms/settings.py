584d05cb878321997db601081360b00db9c2583b
Dmitri Plotnikov <lightsout1305@gmail.com>
2023-07-18 21:29:33 +0300

    THANQA-11 Создал БД на PostgreSQL

--- thankqa_tms/settings.py
+++ thankqa_tms/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'thankqa_tms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('USER'),
+        'PASSWORD': env.str('PASSWORD'),
+        'HOST': env.str('HOST'),
+        'PORT': env.int('PORT')
     }
 }
 
