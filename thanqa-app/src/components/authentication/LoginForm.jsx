47a21bbfd81e7ddd4224bb675b0128a8f4acb1c0
Dmitri Plotnikov <lightsout1305@gmail.com>
2023-08-01 00:18:00 +0300

    THANQA-14 Пофиксил иконку, добавил валидацию форм

--- thanqa-app/src/components/authentication/LoginForm.jsx
+++ thanqa-app/src/components/authentication/LoginForm.jsx
@@ -3,31 +3,30 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {Button} from "./Button";
 import {Input} from "./Input";
-import {Form} from "./Form";
+
 
 function LoginForm() {
     const navigate = useNavigate();
     const [validated, setValidated] = useState(false);
     const [form, setForm] = useState({});
-    const [error, setError ] = useState(null);
+    const [error, setError] = useState(null);
     const inputStyle = {
         marginLeft: "35px",
-        marginBottom: "35px",
-    }
+        marginBottom: "35px"
+    };
     const buttonStyle = {
         marginTop: "52px",
         marginLeft: "130px",
         marginBottom: "90px",
         color: "white",
         fontFamily: "Roboto"
-    }
-
+    };
     const textStyle = {
         marginTop: "35px",
         marginBottom: "35px",
         marginLeft: "153px",
         marginRight: "152px"
-    }
+    };
 
     const iconStyle = {
         marginTop: "75px",
@@ -37,36 +36,72 @@ function LoginForm() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const loginForm = event.currentTarget;
-
-        if (loginForm.checkValidity() === false) {
-            event.stopPropagation();
-        }
 
         setValidated(true);
 
-        const data = {
-            "user": {
-                "email": form.username,
-                "password": form.password,
-            }
-        };
+        if (form.username == null || form.username === "") {
+            event.stopPropagation();
+            form.error = "Email is required";
+            const requiredLogin = document.createElement("p");
+            requiredLogin.style.marginTop = "0px";
+            requiredLogin.style.marginLeft = "35px";
+            requiredLogin.style.marginBottom = "0px";
+            requiredLogin.style.color = "#F00";
+            requiredLogin.style.fontFamily = "Inter";
+            requiredLogin.style.fontSize = "15px";
+            requiredLogin.style.fontStyle = "normal";
+            requiredLogin.style.fontWeight = "400";
+            requiredLogin.style.lineHeight = "normal";
+            requiredLogin.textContent = form.error;
+            document.getElementById("loginInput").style.border = "2px solid #FE0D0D";
+            document.getElementById("loginInput").insertAdjacentElement("beforebegin", requiredLogin);
+            document.getElementById("loginInput").style.marginBottom = "14px";
+        }
 
-        axios
-            .post("http://127.0.0.1:8000/api/users/login/", data)
-            .then((res) => {
-                localStorage.setItem("auth", JSON.stringify({
-                    user: res.data.user.email,
-                    token: res.data.user.token
-                }));
+        if (form.password == null || form.password === "") {
+            event.stopPropagation();
+            form.error = "Password is required";
+            const requiredPassword = document.createElement("p");
+            requiredPassword.style.marginTop = "0px";
+            requiredPassword.style.marginLeft = "35px";
+            requiredPassword.style.marginBottom = "0px";
+            requiredPassword.style.color = "#F00";
+            requiredPassword.style.fontFamily = "Inter";
+            requiredPassword.style.fontSize = "15px";
+            requiredPassword.style.fontStyle = "normal";
+            requiredPassword.style.fontWeight = "400";
+            requiredPassword.style.lineHeight = "normal";
+            requiredPassword.textContent = form.error;
+            document.getElementById("passwordInput").style.border = "2px solid #FE0D0D";
+            document.getElementById("passwordInput").insertAdjacentElement("beforebegin", requiredPassword);
+            document.getElementById("passwordInput").style.marginBottom = "14px";
+        }
 
-                navigate("/");
-            })
-            .catch((err) => {
-                if (err.message) {
-                    setError(err.request.response);
+        else {
+
+            const data = {
+                "user": {
+                    "email": form.username,
+                    "password": form.password,
                 }
-            });
+            };
+
+            axios
+                .post("http://127.0.0.1:8000/api/users/login/", data)
+                .then((res) => {
+                    localStorage.setItem("auth", JSON.stringify({
+                        user: res.data.user.email,
+                        token: res.data.user.token
+                    }));
+
+                    navigate("/");
+                })
+                .catch((err) => {
+                    if (err.message) {
+                        setError(err.request.response);
+                    }
+                });
+            }
     }
 
     const changeButtonColorMouseIn = (event) => {
@@ -82,6 +117,7 @@ function LoginForm() {
             id="loginForm"
             className="loginForm"
             onSubmit={handleSubmit}
+            noValidate
             >
             <div className="sign-in-icon" style={iconStyle}>
 
@@ -103,7 +139,7 @@ function LoginForm() {
                     </Input>
                     <Input
                         id="passwordInput"
-                        type="text"
+                        type="password"
                         placeholder="Enter your password"
                         className="input-qa"
                         value={form.password}
